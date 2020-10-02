eb6166722d5e47bf88115dd5c1ac14ed238bfb60
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-10-03 01:15:27 +0530

    first commit

---
+++ CustomLoginDemo/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  CustomLoginDemo
+//
+//  Created by Prabaljit Walia
+//  Copyright © 2020 Prabaljit Walia. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
